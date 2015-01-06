ceedf2f564f7ed947e2864bc6de42f142f870b43
Scott Pullen <s.pullen05@gmail.com>
2015-01-05 22:06:40 -0500

    initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,11 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+      }
+    }
+  });
+};
